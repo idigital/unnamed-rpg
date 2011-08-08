e2ab077c90b489168f37aee408d0f7196a304afe
Shane Preece <shamess@gmail.com>
2011-08-08 16:25:30 +0100

    Made table background color same as the grass

--- map.draw.php
+++ map.draw.php
@@ -157,10 +157,10 @@ while ($map_array = mysql_fetch_array ($qry_mapgrid)) {
 		$image = $map_array['image'];
 	}
 
-	$map_data_output .= "\t\t\t<td style=\"margin:0; background-color: #00FF01; border-style:none;\"><img src=\"".relroot."/images/map_images/".$image."\" ";
+	$map_data_output .= "\t\t\t<td style=\"margin:0; background-color: #8CDE21; border-style:none;\"><img src=\"".relroot."/images/map_images/".$image."\" ";
 	//  if this is a dev-serv then show the map's coordinants
 	if (STATUS != "LIVE") $map_data_output .= " title=\"x is ".$map_array['x_co'].", y is ".$map_array['y_co']."\" ";
-	$map_data_output .= "alt=\"Map square\" style=\" width: 40px; height: 40px; background-color: #4e5545; \" /></td>\n";
+	$map_data_output .= "alt=\"Map square\" style=\" width: 40px; height: 40px; background-color: #8CDE21; \" /></td>\n";
 
 	// add one to count. this increases with each coord output so we know how many we've gone across the X axis.
 	$in_row++;

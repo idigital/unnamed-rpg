d229afabaf4aca7a5425bf4de1374e306234db5f
Shane Preece <shamess@gmail.com>
2011-08-17 01:24:50 +0100

    Added dummy fight page for testing

---
+++ fight.php
@@ -0,0 +1,12 @@
+<?php
+
+define ('LOGIN', 1);
+
+require_once ('includes/notextinc.php');
+$ext_title = "Fight!";
+include_once ('includes/header.php');
+
+echo "<p>Brb.</p>\n";
+
+include_once ('includes/footer.php');
+?>

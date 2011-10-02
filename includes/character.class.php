f99c01728fdadb860e3728a9217eaae772082490
Shane Preece <shamess@gmail.com>
2011-10-02 02:33:38 +0100

    Changed getFightData based on new mechanics for deciding if they're in a fight.
    Now, a play is in a fight if their phase is set to 'fight'. We assume
    they're in the fight with the highest fight_id. The `complete` attribute
    is used to decide if they should be looking at the aftermath or not.

--- includes/character.class.php
+++ includes/character.class.php
@@ -82,7 +82,7 @@ class Character extends StandardObject {
 	public function getFightData ($fight_id="current") {
 		if ($fight_id == "current") {
 			// just find the last fought fight to return
-			$fight_id = $this->getDatabase()->getSingleValue ("SELECT `fight_id` FROM `user_fight` WHERE `user_id` = ".$this->getId()." AND `complete` = 0");
+			$fight_id = $this->getDatabase()->getSingleValue ("SELECT `fight_id` FROM `user_fight` WHERE `user_id` = ".$this->getId()." ORDER BY `fight_id` DESC LIMIT 1");
 			
 			if (empty ($fight_id)) {
 				return false;

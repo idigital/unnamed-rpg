4a33bdbac3e4d16fa091113e328dab9222b91341
Shane Preece <shamess@gmail.com>
2011-11-21 21:39:39 +0000

    Testing out a new invetory layout.

--- items.php
+++ items.php
@@ -12,21 +12,29 @@ include ('fight_scripts/stat_bar.php');
 
 echo "<h1>Your items</h1>\n";
 
-echo "<p style=\"text-align: center;\"><input type=\"button\" value=\"Click here to return to the map\" onclick=\"window.location = 'map.php'\" /></p>\n";
+echo "<div id=\"characters\">\n";
+echo "<div class=\"player main\">\n";
+echo "<div class=\"portrait\"><img src=\"".relroot."/images/fight/lupe_combat.gif\" /></div>\n";
+echo "<p class=\"name\">".$User->getDetail ('username')."</p>\n";
+// we still have $health_bar set up from the stat_bar we included!
+echo "<p>Health: <strong class=\"char_health\">".$Character->getDetail ('remaining_hp')."</strong>/".$Character->getMaxHealth()." ".$health_bar."</p>\n";
+echo "</div>\n";
+
+echo "<div class=\"clear\">&nbsp;</div>\n";
 
 $Items = $Character->getInventory()->getItems();
 
 if ($Items->count()) {
-	echo "<table style=\"width: 100%;\">\n";
-	echo "<tr><th>Name</th><th>Quantity</th><th>Type</th><th>Actions</th></tr>\n";
+	echo "<h2>Inventory</h2>\n";
+	echo "<ul id=\"items\">\n";
 	foreach ($Items as $Il) {
-		echo "<tr>";
-		echo "<td>". ucfirst ($Il['Item']->getName ()) ."</td>";
-		echo "<td><b>".$Il['qty']."</b>/".$Il['Item']->getMaxQty()."</td>";
-		echo "<td>".$Il['Item']->getDetail ('type')."</td>";
-		echo "</tr>\n";
+		echo "<li class=\"item\">";
+		echo ucfirst ($Il['Item']->getName ());
+		echo "</li>\n";
 	}
-	echo "</table>\n";
+	echo "</ul>\n";
+	
+	echo "<div class=\"clear\">&nbsp;</div>\n";
 } else {
 	echo "<p>You don't have any items to use.</p>\n";
 }
